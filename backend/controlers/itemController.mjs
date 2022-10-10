0db6bc21fcfdb7f83232ada12bb2ecaccffd29ee
Viikuu <viiktorw77@gmail.com>
2022-10-10 13:01:59 +0200

    Item model refactor and item creation function

---
+++ backend/controlers/itemController.mjs
@@ -0,0 +1,35 @@
+import {collAuthor} from '../middleware/collAuthor.mjs';
+import {ItemModel} from '../models/itemModel.mjs';
+import {CollectionModel} from '../models/collectionModel.mjs';
+
+const createItem = async (request, response, next) => {
+	try {
+		const {collectionId, item} = request.body;
+		const collection = await CollectionModel.find({_id:collectionId});
+		if(await collAuthor(collection, request._id)) {
+			for(const tag in collection.tags) {
+				if(item[tag] === undefined) {
+					return response.json({state: false, message: ''});
+				}
+			}
+			const {name, tags, other} = item;
+
+			await ItemModel.create(
+				{
+					collectionId,
+					name: item.name,
+					tags: item.tags,
+					other: other,
+				});
+			return response.json({state: true});
+		}
+		return response.json({state: true, message: 'Unauthorized'});
+
+	} catch (error) {
+		next(error);
+	}
+}
+
+export {
+	createItem,
+};
