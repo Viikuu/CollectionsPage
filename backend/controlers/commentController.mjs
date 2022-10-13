88970bee67e5d3dfac849e92c45b311364f53847
Viikuu <viiktorw77@gmail.com>
2022-10-13 12:21:24 +0200

    REST controller creation for Comment REST api

---
+++ backend/controlers/commentController.mjs
@@ -0,0 +1,66 @@
+import {CommentModel} from '../models/commentModel.mjs';
+
+const createComment = async (request, response, next) => {
+	try {
+		const {comment, itemId} = request.body;
+		await CommentModel.create(
+			{
+				userId: request._id,
+				itemId,
+				content: comment,
+			});
+		return response.json({state: true});
+	} catch (error) {
+		next(error);
+	}
+}
+
+const getCommentsByItemId = async (request, response, next) => {
+	try {
+		const {itemId} = request.params;
+		const comments = await CommentModel.find({itemId:itemId});
+		return response.json({state: true,data: comments});
+	} catch (error) {
+		next(error);
+	}
+}
+
+const updateComment = async (request, response, next) => {
+	try {
+		const {content} = request.body;
+		const {commentId} = request.params;
+		const comment = await CommentModel.find({_id: commentId});
+		if(comment.userId === request._id) {
+			await CommentModel.findByIdAndUpdate({
+					_id:commentId,
+				}, {
+					content,
+				});
+			return response.json({state: true});
+		}
+		return response.json({state: true, message: 'Unauthorized'});
+	} catch (error) {
+		next(error);
+	}
+}
+
+const deleteComment = async (request, response, next) => {
+	try {
+		const {commentId} = request.params;
+		const comment = await CommentModel.find({_id: commentId});
+		if(comment.userId === request._id) {
+			await CommentModel.findByIdAndDelete(commentId);
+			return response.json({state: true});
+		}
+		return response.json({state: true, message: 'Unauthorized'});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export {
+	getCommentsByItemId,
+	createComment,
+	deleteComment,
+	updateComment,
+}
