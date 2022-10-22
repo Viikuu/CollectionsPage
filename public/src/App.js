8818c5da13ade38acfe122016133cb60f4144b7e
Viikuu <viiktorw77@gmail.com>
2022-10-22 13:47:04 +0200

    Frontend page routes specified

--- public/src/App.js
+++ public/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {MainPage} from './pages/MainPage';
+import {Register} from './pages/Register';
+import AdminPanel from './pages/AdminPanel';
+import {Login} from './pages/Login';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element = {<MainPage />} />
+            <Route path={"/"} element = {<MainPage />} />
+            <Route path={"/Register"} element = {<Register />} />
+            <Route path={"/Login"} element = {<Login />} />
+            <Route path={"/Admin"} element = {<AdminPanel />} />
         </Routes>
       </BrowserRouter>
   );
