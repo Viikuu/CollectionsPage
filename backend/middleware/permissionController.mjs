4f20138cad385dcdec402c43a2c716275b8e4308
Viikuu <viiktorw77@gmail.com>
2022-10-23 16:59:48 +0200

    isAdmin middleware implementation to find out if user is admin

--- backend/middleware/permissionController.mjs
+++ backend/middleware/permissionController.mjs
@@ -1,10 +1,25 @@
 import {UserModel} from '../models/userModel.mjs';
 
-export async function collAuthor (collection, userid) {
+async function collAuthor (collection, userid) {
 	const user = await UserModel.find({_id:userid}).select([
 		'name',
 		'rank',
 	]);
 	return collection.author === user.name || user.rank === 'admin';
+}
+
+async function isAdmin (request, response, next) {
+	const user = await UserModel.find({_id:request.id}).select([
+		'rank',
+	]);
+	if(user.rank === 'admin'){
+		next();
+	}
+	response.status(401).json({state: false, message: 'Unauthorized'});
+}
+
+export {
+	collAuthor,
+	isAdmin,
 }
 
