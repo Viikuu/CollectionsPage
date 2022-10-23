3ca51d17e9ee0cbd2bf01d3e62b433250e44bf19
Viikuu <viiktorw77@gmail.com>
2022-10-23 17:01:48 +0200

    Checking if user is logged in to access userRoutes and Blocking endpoints for users without admin permissions

--- backend/routers/userRoutes.mjs
+++ backend/routers/userRoutes.mjs
@@ -6,14 +6,16 @@ import {
 	unBlockUsers,
 	user,
 } from '../controlers/usersController.mjs';
+import {authenticateToken} from '../middleware/authMiddleware.mjs';
+import {isAdmin} from '../middleware/permissionController.mjs';
 
 const userRoutes = Router();
 
-userRoutes.get('/', user);
-userRoutes.get('/allusers', allusers);
-userRoutes.put('/blockusers', blockUsers);
-userRoutes.put('/unblockusers', unBlockUsers);
-userRoutes.post('/', deleteUsers);
+userRoutes.get('/', authenticateToken, user);
+userRoutes.get('/allusers', authenticateToken, allusers);
+userRoutes.put('/blockusers', authenticateToken, isAdmin, blockUsers);
+userRoutes.put('/unblockusers', authenticateToken, isAdmin, unBlockUsers);
+userRoutes.post('/', authenticateToken, isAdmin, deleteUsers);
 
 export {
 	userRoutes,
